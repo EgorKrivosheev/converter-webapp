a06f1b6d4488de29df6b09ff86f3cfac71a5a736
EgorKrivosheev <EgorKrivosheev@users.noreply.github.com>
2021-02-02 12:28:49 +0300

    [0.8.3]
    Fixed pom.xml, style.css

--- src/main/webapp/resources/scripts/converterApp.js
+++ src/main/webapp/resources/scripts/converterApp.js
@@ -2,5 +2,5 @@
 
 const _CONVERTER_APP = angular.module("converterApp", [])
     .run(function ($rootScope) {
-        $rootScope.version = "version: 0.8.2";
+        $rootScope.version = "version: 0.8.3";
     });
